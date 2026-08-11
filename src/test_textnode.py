2f337b067e2c754f17f6325dffbb68ca8cb9ddce
Vali <valy.tyno@gmail.com>
2026-08-11 18:05:41 +0300

    G: text node to html node

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,5 +1,5 @@
 import unittest
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -20,6 +20,19 @@ class TestTextNode(unittest.TestCase):
     def test_no_url(self):
         node = TextNode("This is a text node", TextType.PLAIN)
         self.assertIsNone(node.url)
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.PLAIN)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def text_image(self):
+        node = TextNode("This is an image", TextType.IMAGE, url="https://example.com/image.png")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.props["src"], "https://example.com/image.png")
+        self.assertEqual(html_node.props["alt"], "This is an image")
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,7 @@
 from enum import Enum
-from typing import Union
+from typing import Set, Union
+
+from htmlnode import LeafNode
 
 class TextType(Enum):
   PLAIN="text"
@@ -22,3 +24,26 @@ class TextNode():
 
   def __repr__(self):
     return f"TextNode(text={self.text}, text_type={self.text_type.value}, url={self.url})"
+  def __repr__(self):
+    return f"TextNode(text={self.text}, text_type={self.text_type.value}, url={self.url})"
+
+
+def text_node_to_html_node(text_node: 'TextNode') -> LeafNode:
+  if not isinstance(text_node.text_type, TextType):
+    raise ValueError(f"Invalid text type: {text_node.text_type}")
+  if text_node.text_type == TextType.PLAIN:
+    return LeafNode(tag=None, value=text_node.text)
+  elif text_node.text_type == TextType.BOLD:
+    return LeafNode(tag="b", value=text_node.text)
+  elif text_node.text_type == TextType.ITALIC:
+    return LeafNode(tag="i", value=text_node.text)
+  elif text_node.text_type == TextType.CODE:
+    return LeafNode(tag="code", value=text_node.text)
+  elif text_node.text_type == TextType.LINK:
+    if not text_node.url:
+      raise ValueError("Link text node must have a URL")
+    return LeafNode(tag="a", value=text_node.text, props={"href": text_node.url})
+  elif text_node.text_type == TextType.IMAGE:
+    if not text_node.url:
+      raise ValueError("Image text node must have a URL")
+    return LeafNode(tag="img", value=None, props={"src": text_node.url, "alt": text_node.text})
